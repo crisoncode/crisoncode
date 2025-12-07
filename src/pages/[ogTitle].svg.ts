1ef4f527c855b87687bb3993cbd2d93f63ef9a1d
Cristian Estarlich <cestarlich@tennis-point.es>
2025-12-07 18:52:58 +0100

    updating node

--- src/pages/[ogTitle].svg.ts
+++ src/pages/[ogTitle].svg.ts
@@ -2,17 +2,19 @@ import { getCollection } from "astro:content";
 import generateOgImage from "@utils/generateOgImage";
 import type { APIRoute } from "astro";
 
-export const get: APIRoute = async ({ params }) => ({
-  body: await generateOgImage(params.ogTitle),
-});
+export const GET: APIRoute = async ({ params }) => {
+	return new Response(await generateOgImage(params.ogTitle), {
+		headers: { "Content-Type": "image/svg+xml" },
+	});
+};
 
 const postImportResult = await getCollection("blog", ({ data }) => !data.draft);
 const posts = Object.values(postImportResult);
 
 export function getStaticPaths() {
-  return posts
-    .filter(({ data }) => !data.ogImage)
-    .map(({ data }) => ({
-      params: { ogTitle: data.title },
-    }));
+	return posts
+		.filter(({ data }) => !data.ogImage)
+		.map(({ data }) => ({
+			params: { ogTitle: data.title },
+		}));
 }
